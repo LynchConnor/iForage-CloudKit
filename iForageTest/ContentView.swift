2f618ad5c0baf5eef36fe3cf4db85c70ab466a6d
CloseBuyUK <86831782+CloseBuyUK@users.noreply.github.com>
2021-11-25 22:12:24 +0000

    Initial Commit

---
+++ iForageTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iForageTest
+//
+//  Created by Connor A Lynch on 25/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iForageTest/iForageTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iForageTestApp.swift
+//  iForageTest
+//
+//  Created by Connor A Lynch on 25/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct iForageTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
